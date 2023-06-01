4730375aeb9c1d581751d5991f294814d81c5c6e
Анастасия Маевская <Quri.mau@gmail.com>
2023-06-01 22:23:12 +0300

    2iterRouter

--- routes/router.js
+++ routes/router.js
@@ -1,14 +1,22 @@
 /* eslint-env es6 */
 const router = require("express").Router();
 
-const auth = require('../middleweares/auth');
+const auth = require("../middleweares/auth");
 
 const cardsRouter = require("./cards");
 const usersRouter = require("./users");
 
+const {
+  createUserValidator,
+  loginValidator,
+} = require("../middleweares/validation");
+
+const { createUser, login } = require("../controllers/users");
+
 const NOT_FOUND_ERROR = require("../errors/notFoundError");
 
-
+router.post("/signin", loginValidator, login);
+router.post("/signup", createUserValidator, createUser);
 
 router.use("/users", auth, usersRouter);
 router.use("/cards", auth, cardsRouter);
